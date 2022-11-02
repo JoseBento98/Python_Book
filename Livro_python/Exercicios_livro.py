d9db732f467c1770cec945d966d5435bb1dafcfb
José Bento <jldbento@gmail.com>
2022-11-02 12:23:52 -0300

    Atualização dos Exercicios

--- Livro_python/Exercicios_livro.py
+++ Livro_python/Exercicios_livro.py
@@ -35,9 +35,89 @@ earnings_per_hour = (remnuneration / worked_days) / worked_hours
 
 print(f"Sr(a) {first_name} {second_name} ganha aproximadamente {earnings_per_hour:2.2f} $ por Hora trabalhada!")
 print("Seu Nick Name é: ", first_name, age, sep="#",end="@" )
+print("Carregando...")
+time.sleep(1)
+
+    #Operadores de comparação
+BALANCE = 1000
+WITHDRAW = 1000
+
+print(BALANCE == WITHDRAW) #Igualdade
+print(BALANCE != WITHDRAW) #Diferença
+print(BALANCE > WITHDRAW) #Maior_Que
+print(BALANCE >= WITHDRAW) #Maior_ou_Igual
+print(BALANCE < WITHDRAW) #Menor_que
+print(BALANCE <= WITHDRAW) #Menor_ou_igual
+
+    #Atribuição
+    #red_stone = 100 #simples
+    #red_stone += 100 #atribuição com Adição
+
+    #red_stone = 100 #Com_Subtração
+    #red_stone -= 100
+
+    #red_stone = 100 #Com_Multiplicação
+    #red_stone *= 100
+
+    #red_stone = 100 #Com_divisão
+    #red_stone /= 100
+
+    #red_stone = 100 #Com_Sdivisão inteira
+    #red_stone //= 100
+
+    #red_stone = 100 #Com_Modulo(Resto da divisão)
+    #red_stone %= 100
+
+    #red_stone = 100 #Com_exponenciação(Square_Root)
+    #red_stone **= 100
+
+    #Comparação (e) AND
+    #as 2 condições tem que ser TRUE para que a comparação sera TRUE
+BALANCE = 1000
+WITHDRAW = 1000
+LIMIT = 1200
+
+BALANCE >= WITHDRAW and WITHDRAW <= LIMIT
+
+    #Comparação (ou) or
+    #uma das condições sendo TRUE o resultado sera TRUE
+    #preciso que as 2 seja FALSE para dar FALSE
+BALANCE = 1000
+WITHDRAW = 1000
+LIMIT = 1200
+
+BALANCE >= WITHDRAW or WITHDRAW <= LIMIT
+
+    #comparação (negação) not
+    #inverte os valores das condições de TRUE para FALSE...
+BALANCE = 1000
+WITHDRAW = 1000
+LIMIT = 1200
+
+not BALANCE > LIMIT
+
+    #Operador de Identidade IS (é ou não é)
+#FOOD = "Barbecue"
+#MY_FAVORITE_FOOD = FOOD
+
+#FOOD is MY_FAVORITE_FOOD
+#FOOD is not MY_FAVORITE_FOOD
+
+    #Operações Associadas diz se é True Or False elementos pesquisados em listas
+profit = "my profit of the month"
+food = ["meat", "fruits", "vegetables"]
+money = [500,600,1200]
+
+print("month" in profit)
+
+print("Barbecue" not in food)
+
+print(700 in money) 
 
 
 #Condições
+
+
 #Repetições
 #Lista,Dicionarios,tupulas e conjuntos
 #Trabalhando com Strings
